339bf1480aff5a77bbe7ae68264e7476877d2a3d
cjlangan <connorjameslangan@gmail.com>
2024-09-18 15:49:02 -0500

    refactored ID bot to automatically export environment variable

--- get-id.py
+++ get-id.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import subprocess
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
@@ -9,10 +10,18 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_id(message):
     chat_id = message.chat.id
     bot.reply_to(message, f"Your chat id is: {chat_id}")
-    
-    print(f"Your chat id is: {chat_id}")
-    
+
     # Stop the bot from checking for messages
     bot.stop_polling() 
+    
+    # Export chat ID to an environment variable
+    export = f"""echo 'export CHAT_ID="{chat_id}"' >> ~/.bashrc"""
+    source = "source ~/.bashrc"
+    subprocess.run(export, shell=True)
+    subprocess.run(source, shell=True)
+
+    # Give user information
+    print(f"Your chat id is: {chat_id}, and has been exported to your path")
+    
 
 bot.polling(timeout=1, long_polling_timeout=1)
