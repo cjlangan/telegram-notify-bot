1413b9988ba397f76fa84e6cc87302019352a3c9
cjlangan <connorjameslangan@gmail.com>
2024-09-18 16:15:39 -0500

    updated comments and funciton names

--- install.py
+++ install.py
@@ -5,16 +5,17 @@ import subprocess
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# Function to send the chat id.
+# Function to get id and install the software
 @bot.message_handler(func=lambda message: True)
-def send_id(message):
+def install(message):
+    # Send chat id inside telegram.  THis is technically not needed.
     chat_id = message.chat.id
     bot.reply_to(message, f"Your chat id is: {chat_id}")
 
     # Stop the bot from checking for messages
     bot.stop_polling() 
     
-    # Export chat ID to an environment variable
+    # Export chat ID to an environment variable and add the notify script to the path
     export = f"""echo 'export CHAT_ID="{chat_id}"' >> ~/.bashrc"""
     source = "source ~/.bashrc"
     add_path = "sudo cp notify.py /usr/local/bin/notify"
